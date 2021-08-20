00185e6fc6a8e98050f478a117ebc12572dcd4b1
9999cn <42468683+9999cn@users.noreply.github.com>
2021-08-20 19:20:05 +0800

    Delete 91.js

--- Quantumult/Rewrite/91.js
+++
@@ -1,2 +0,0 @@
-^https?:\/\/.+?\.(my10api|(.*91.*))\.(com|tips|app|xyz)(:\d{2,5}|)\/api.php$ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/91.js
-hostname = *.my10api.com, *.91apiapi.com, *.bt0577.com, *.vvvcms.com, *.flurry.com, *.klhhgsc.com, *.i91porn.xyz, *.91-tv.me
