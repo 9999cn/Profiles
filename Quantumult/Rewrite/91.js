626ef6fba24e37ae613a1ec07e6fce2d4055bedc
9999cn <42468683+9999cn@users.noreply.github.com>
2021-08-20 18:28:58 +0800

    Create 91.js

---
+++ Quantumult/Rewrite/91.js
@@ -0,0 +1,2 @@
+^https?:\/\/.+?\.(my10api|(.*91.*))\.(com|tips|app|xyz|tv|me)(:\d{2,5}|)\/api.php$ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/91.js
+hostname = *.my10api.com, *.91apiapi.com, *.bt0577.com, *.vvvcms.com, *.flurry.com, *.klhhgsc.com, *.i91porn.xyz, *.91-tv.me
