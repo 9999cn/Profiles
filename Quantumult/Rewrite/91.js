1d345eaa7737ae08a5a98c2895218359c4b170ab
9999cn <42468683+9999cn@users.noreply.github.com>
2021-08-20 18:32:04 +0800

    Update 91.js

--- Quantumult/Rewrite/91.js
+++ Quantumult/Rewrite/91.js
@@ -1,2 +1,3 @@
 ^https?:\/\/.+?\.(my10api|(.*91.*))\.(com|tips|app|xyz|tv|me)(:\d{2,5}|)\/api.php$ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/91.js
-hostname = *.my10api.com, *.91apiapi.com, *.bt0577.com, *.vvvcms.com, *.flurry.com, *.klhhgsc.com, *.i91porn.xyz, *.91-tv.me
+# hostname = *.my10api.com, *.91apiapi.com, *.bt0577.com, *.vvvcms.com, *.flurry.com, *.klhhgsc.com, *.i91porn.xyz, *.91-tv.me
+force-http-engine-hosts = *91*:8080, *my10api*:8080
